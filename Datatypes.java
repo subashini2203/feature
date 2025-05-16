ece352be8325c2a363570ed7a433414c819a1cb2
subashini2203 <subashininaveen2203@gmail.com>
2025-05-16 15:57:39 +0530

    primitive datatypes

---
+++ Datatypes.java
@@ -0,0 +1,17 @@
+public class Datatypes {
+    public static void main(String[] args){
+    int age=25;
+    float height=5.2f;
+    double weight=3.141;
+    long phoneNumber=9080676752L;
+    char grade='A';
+    String name="john";
+    System.out.println("Name:"+name);
+    System.out.println("Age:"+age);
+    System.out.println("Height:"+height+"feet");
+    System.out.println("Weight:"+weight+"kg");
+    System.out.println("PhoneNumber:"+phoneNumber);
+    System.out.println("Grade:"+grade);
+
+    }   
+}
